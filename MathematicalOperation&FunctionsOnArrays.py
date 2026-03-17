700fff15e36c9169a0fb28e02a55adabc7c9cbb0
Debangshi Dasgupta <debangshidasgupta@gmail.com>
2026-03-18 00:07:33 +0530

    Mathematical Operation and functions on arrays

---
+++ MathematicalOperation&FunctionsOnArrays.py
@@ -0,0 +1,37 @@
+import numpy as np
+#Addition-- a+b , p.add(a,b)
+arr1 = np.array([20,25,23,40])
+arr2 = np.array([10,19,68,29])
+print(arr1+arr2)
+print(np.add(arr1,arr2))
+
+#Subtraction-- a-b , p.subtract(a,b)
+arr3 = np.array([50,56,70,68])
+arr4 = np.array([30,32,48,54])
+print(arr3-arr4)
+print(np.subtract(arr3,arr4))
+
+#Multiplication-- a*b , p.multiply(a,b)
+arr5 = np.array([5,6,7,8])
+arr6 = np.array([2,3,4,5])
+print(arr5*arr6)
+print(np.multiply(arr5,arr6))
+
+#Division-- a/b , p.divide
+arr7 = np.array([20,45,60,30])
+arr8 = np.array([10,15,6,10])
+print(arr7/arr8)
+print(np.divide(arr7,arr8))
+
+#Power
+arr9 = np.array([3,4,2,1])
+arr10 = np.array([2])
+print(np.power(arr9,arr10))
+
+#Square root
+arr11 = np.array([9,16,4,1])
+print(np.sqrt(arr11))
+
+
+
+
